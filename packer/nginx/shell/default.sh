7bcdf01a7b02d15b2e402d444aa9a0fe502648b8
Adarsh Shah <shahadarsh@gmail.com>
2017-10-28 23:43:06 -0400

    Added goss tests for packer nginx

--- packer/nginx/shell/default.sh
+++ packer/nginx/shell/default.sh
@@ -5,3 +5,6 @@ set -e
 sudo apt-get update
 sudo apt-get install -y python-pip
 sudo pip install ansible
+
+sudo curl -L https://github.com/aelsabbahy/goss/releases/download/v0.3.5/goss-linux-amd64 -o /usr/local/bin/goss
+sudo chmod +rx /usr/local/bin/goss
