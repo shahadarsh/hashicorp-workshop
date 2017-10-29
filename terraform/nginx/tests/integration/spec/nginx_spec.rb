a32b99e96c81de8fa4c20f839386591149487094
Adarsh Shah <shahadarsh@gmail.com>
2017-10-29 01:21:19 -0400

    Added Serverspec integration tests

---
+++ terraform/nginx/tests/integration/spec/nginx_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe security_group('sg_nginx'), region: 'us-east-1' do
+  it { should exist}
+end
+
+describe ec2('nginx-0'), region: 'us-east-1' do
+  it { should be_running }
+  it { should have_security_group('sg_nginx') }
+end
+
+describe ec2('nginx-1'), region: 'us-east-1' do
+  it { should be_running }
+  it { should have_security_group('sg_nginx') }
+end
+
+describe ec2('nginx-2'), region: 'us-east-1' do
+  it { should be_running }
+  it { should have_security_group('sg_nginx') }
+end
